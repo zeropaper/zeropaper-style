0a12bbd972e26cf7cc52f8e962bed82385fd2d68
Valentin Vago <zeropaper@irata.ch>
2020-11-28 06:40:33 +0100

    disable service worker for apache2 debugging

--- gatsby-config.js
+++ gatsby-config.js
@@ -97,8 +97,8 @@ module.exports = {
       },
     },
 
-    // // this (optional) plugin enables Progressive Web App + Offline functionality
-    // // To learn more, visit: https://gatsby.dev/offline
-    'gatsby-plugin-offline',
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // 'gatsby-plugin-offline',
   ],
 };
