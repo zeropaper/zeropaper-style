1de1b4bd0bba61e79ebab6e1785293e925dda61d
Valentin Vago <zeropaper@irata.ch>
2020-11-29 09:04:26 +0100

    remove js redirection comment

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,18 +8,6 @@ const {
   default: AssetsProvider,
 } = require('./src/components/AssetsProvider/AssetsProvider');
 
-// const {
-//   hostname,
-//   protocol,
-//   host,
-//   pathname,
-//   search,
-//   hash,
-// } = window.location;
-// if (hostname !== 'localhost' && protocol !== 'https:') {
-//   window.location.href = `https://${host}${pathname}${search}${hash}`;
-// }
-
 // eslint-disable-next-line react/prop-types
 exports.wrapRootElement = ({ element }) => (
   <AssetsProvider>
