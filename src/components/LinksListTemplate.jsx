1f809d09477aa0bce6f854dfa3c8dfe771c82297
Valentin Vago <zeropaper@irata.ch>
2020-12-20 10:47:37 +0100

    work on blog listing

--- src/components/LinksListTemplate.jsx
+++ src/components/LinksListTemplate.jsx
@@ -1,27 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import { createUseStyles } from 'react-jss';
 
 import Layout from './Layout/Layout';
 import Link from './Link/Link';
+import TagsList from './TagsList/TagsList';
 
-const useStyles = createUseStyles({
-  root: {},
-  topLevelList: {},
-  itemRoot: {},
-}, {
+const useStyles = createUseStyles(({
+  spacing,
+}) => ({
+  root: {
+    listStyle: 'none',
+    padding: 0,
+    margin: 0,
+  },
+  topLevelList: {
+    listStyle: 'none',
+    padding: 0,
+    margin: 0,
+    marginBottom: spacing(4),
+  },
+  subList: {
+    paddingLeft: spacing(2),
+  },
+  itemRoot: {
+    listStyle: 'none',
+    padding: 0,
+    margin: 0,
+    marginBottom: spacing(3),
+  },
+}), {
   name: 'LinksListTemplate',
 });
 
 export const ListItem = (props) => {
-  const { to, title } = props;
+  const {
+    to,
+    title,
+    date,
+    tags,
+    titleComponent: TitleComponent = 'h2',
+  } = props;
   const classes = useStyles(props);
-
   return (
     <li key={to} className={classes.itemRoot}>
-      <Link to={to}>
-        {title}
-      </Link>
+      {date && (<div>{moment(date).format('MMMM Do, YYYY')}</div>)}
+
+      <TitleComponent>
+        <Link to={to}>
+          {title}
+        </Link>
+      </TitleComponent>
+
+      <TagsList tags={tags} />
     </li>
   );
 };
@@ -29,6 +61,15 @@ export const ListItem = (props) => {
 ListItem.propTypes = {
   to: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
+  date: PropTypes.string,
+  tags: PropTypes.arrayOf(PropTypes.string),
+  titleComponent: PropTypes.elementType,
+};
+
+ListItem.defaultProps = {
+  date: null,
+  tags: [],
+  titleComponent: 'h2',
 };
 
 const LinksListTemplate = (props) => {
@@ -50,12 +91,28 @@ const LinksListTemplate = (props) => {
         ? list.map(ListItem)
         : (
           <ul className={classes.topLevelList}>
-            {Object.keys(list).map((key) => (
+            {Object.keys(list).sort((a, b) => {
+              const lcA = a.toLowerCase();
+              const lcB = b.toLowerCase();
+              if (lcA > lcB) return 1;
+              if (lcA < lcB) return -1;
+              return 0;
+            }).map((key) => (
               <li key={key}>
-                <ul>
-                  <h2>{key}</h2>
+                <h2>{key}</h2>
 
-                  {list[key].map(ListItem)}
+                <ul className={classes.subList}>
+                  {list[key].map(({
+                    to,
+                    title,
+                  }) => (
+                    <ListItem
+                      to={to}
+                      title={title}
+                      titleComponent="h3"
+                      key={to}
+                    />
+                  ))}
                 </ul>
               </li>
             ))}
