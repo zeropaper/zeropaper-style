9dff36bf845c8dbd2d06a905ab53cac74b50c25a
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2022-03-07 11:46:27 +0100

    feat: persist color scheme and hotkey
    mod+j to toggle between light and dark mode

--- themes/Theme.tsx
+++ themes/Theme.tsx
@@ -1,19 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { MantineProvider, ColorScheme, ColorSchemeProvider, useMantineTheme, MantineProviderProps } from '@mantine/core';
+import { useHotkeys, useLocalStorageValue, useColorScheme } from '@mantine/hooks';
 
 export const ThemeProvider = ({ children, ...props }: MantineProviderProps) => {
-  const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+  const preferredColorScheme = useColorScheme();
+
+  const [colorScheme, setColorScheme] = useLocalStorageValue<ColorScheme>({
+    key: 'zps-color-scheme',
+    defaultValue: preferredColorScheme,
+  });
+
   const toggleColorScheme = (value?: ColorScheme) =>
     setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
 
+  useHotkeys([['mod+J', () => toggleColorScheme()]]);
+
   return (
     <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-      <MantineProvider {...props} theme={{ colorScheme }}>
+      <MantineProvider {...props} theme={{ colorScheme }} emotionOptions={{ key: 'zps', prepend: true }}>
         {children}
       </MantineProvider>
     </ColorSchemeProvider>
   );
-
 }
 
 export default ThemeProvider;
