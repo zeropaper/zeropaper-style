890b5b369b7075de84b19c850e18e8e72aa13828
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2022-03-10 06:50:26 +0100

    chore(IFrame): work on styles, add open prop

--- components/IFrame/Iframe.tsx
+++ components/IFrame/Iframe.tsx
@@ -1,6 +1,6 @@
 import React, { PropsWithChildren } from 'react';
 import classNames from 'classnames';
-import { createStyles as createUseStyles } from '@mantine/core';
+import { createStyles as createUseStyles, CSSObject } from '@mantine/core';
 import { useResizeObserver } from '@mantine/hooks';
 
 import Link from '../Link/Link';
@@ -15,6 +15,7 @@ export interface PropTypes {
   source?: string;
   mdx?: TinaMarkdownContent | TinaMarkdownContent[];
   tags?: string[];
+  open?: boolean;
 }
 
 // https://mantine.dev/theming/mantine-provider/#normalizecss-and-global-styles
@@ -25,143 +26,139 @@ const useStyles = createUseStyles(({
   colorScheme,
   spacing,
   primaryColor,
-}) => ({
-  root: {
+}) => {
+  const transitionBase: CSSObject = {
+    transition: 'all',
+    transitionDuration: '324ms',
+    transitionTimingFunction: 'ease-in-out',
+  };
+  const clearSpace: CSSObject = {
     margin: 0,
     padding: 0,
-    width: '100%',
-    flexGrow: 1,
-    position: 'relative',
-    overflow: 'hidden',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  iframeWrapper: {
-    '&:before': {
-      pointerEvents: 'none',
+  };
+  const effectColor = colors[primaryColor][4]
+  return {
+    root: {
+      ...clearSpace,
+      width: '100%',
+      flexGrow: 1,
+      position: 'relative',
+      overflow: 'hidden',
+      display: 'flex',
+      flexDirection: 'column',
+    },
+    iframeWrapper: {
+      '&:before': {
+        ...transitionBase,
+        pointerEvents: 'none',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        content: '""',
+        display: 'block',
+        zIndex: 2,
+        boxShadow: `0 0 0 0 transparent inset`,
+        background: 'transparent',
+      },
+      ...clearSpace,
+      ...transitionBase,
+      overflow: 'hidden',
+      border: 'none',
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      flexGrow: 1,
+      position: 'relative',
+      zIndex: 1,
+    },
+    iframeWrapperShadow: {
+      '&:before': {
+        boxShadow: `0 0 7px 3px ${fn.rgba(effectColor, 0.15)} inset`,
+        background: fn.rgba(effectColor, 0.05)
+      }
+    },
+    iframe: {
+      ...clearSpace,
+      border: 'none',
+      width: '100%',
+      height: '100%',
+      flexGrow: 1,
+      position: 'relative',
+      zIndex: 0,
+    },
+    main: {},
+    scrollable: {
+      overflow: 'auto',
+      padding: spacing.md,
+      background: colorScheme === 'light' ? white : colors.dark[7],
+      maxHeight: '100%',
+      minHeight: '100%',
+      position: 'relative',
+      zIndex: 1,
+    },
+    aside: {
       position: 'absolute',
       top: 0,
-      left: 0,
-      right: 0,
       bottom: 0,
-      content: '""',
-      display: 'block',
-      zIndex: 2,
-      boxShadow: `0 0 7px 0 transparent inset`,
-      transition: 'all 218ms ease-in',
-      // background: 'lime'
+      zIndex: 100,
+      ...transitionBase,
+      transition: 'right',
+      width: '35vw',
+      right: '-35vw',
+      [fn.smallerThan('xs')]: {
+        // TODO: should be stacked below the iframe
+        width: '250px',
+        right: '-250px',
+      },
+      [fn.smallerThan('md')]: {
+        width: '55vw',
+        right: '-55vw',
+      },
+      '&.asideOpen': {
+        right: 0,
+      }
     },
-    overflow: 'hidden',
-    border: 'none',
-    margin: 0,
-    padding: 0,
-    width: '100%',
-    height: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    flexGrow: 1,
-    position: 'relative',
-    zIndex: 1,
-    transition: 'all 218ms ease-in',
-    // transform: 'scale(1, 1)',
-    // transformOrigin: 'center',
-  },
-  iframeWrapperShadow: {
-    '&:before': {
-      boxShadow: `0 0 7px 3px ${fn.rgba(colorScheme === 'dark' ? colors.gray[1] : colors.dark[7], 0.15)} inset`,
-    }
-    // transform: 'scale(0.98, 0.98)',
-  },
-  iframe: {
-    border: 'none',
-    margin: 0,
-    padding: 0,
-    width: '100%',
-    height: '100%',
-    flexGrow: 1,
-    position: 'relative',
-    zIndex: 0,
-  },
-  main: {},
-  scrollable: {
-    overflow: 'auto',
-    padding: spacing.md,
-    background: colorScheme === 'light' ? white : colors.dark[7],
-    maxHeight: '100%',
-    minHeight: '100%',
-    position: 'relative',
-    zIndex: 1,
-  },
-  aside: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    zIndex: 100,
-    transition: 'right 218ms ease-in',
-    borderLeft: '1px solid transparent',
-    width: '35vw',
-    right: '-35vw',
-    [fn.smallerThan('xs')]: {
-      // TODO: should be stacked below the iframe
-      width: '250px',
-      right: '-250px',
-    },
-    [fn.smallerThan('md')]: {
-      width: '55vw',
-      right: '-55vw',
-    },
-    '&.asideOpen': {
+    asideOpen: {
       right: 0,
-    }
-  },
-  asideOpen: {
-    // borderLeft: `1px solid ${colors?.[primaryColor]?.[5] || 'lime'}`,
-    right: 0,
-    // [fn.smallerThan('xs')]: {
-    //   right: 0,
-    // },
-    // [fn.smallerThan('md')]: {
-    //   right: 0,
-    // },
-  },
-  toggleButtonHolder: {
-    position: 'absolute',
-    right: '100%',
-    top: '50%',
-    zIndex: 0,
-  },
-  toggleButton: {
-    // position: 'relative',
-    // zIndex: 1,
-    // borderRadius: `${spacing.xs}px 0 0 ${spacing.xs}px`,
-    borderRadius: 0,
-    transformOrigin: 'bottom center',
-    transform: `translate(calc(50% + 1px), calc(((${spacing.xs}px * 2) + 1rem) * -1)) rotate(-90deg)`,
-    border: 'none',
-    // borderColor: colors?.[primaryColor]?.[5] || 'lime',
-    // borderWidth: '1px 0 1px 1px',
-    cursor: 'pointer',
-    paddingTop: spacing.xs,
-    paddingBottom: spacing.xs,
-    paddingLeft: spacing.sm,
-    paddingRight: spacing.sm,
-    background: colorScheme === 'light' ? white : colors.dark[7],
-    color: 'inherit',
-    outline: 'none',
-    boxShadow: `0 0 7px 0 transparent`,
-    transition: 'all 218ms ease-in',
-    '&:hover': {
-      boxShadow: `0 0 7px 3px ${fn.rgba(colorScheme === 'dark' ? colors.gray[1] : colors.dark[7], 0.15)}`,
-    }
-  },
-}));
+    },
+    toggleButtonHolder: {
+      position: 'absolute',
+      right: '100%',
+      top: '50%',
+      zIndex: 0,
+    },
+    toggleButton: {
+      borderRadius: 0,
+      transformOrigin: 'bottom center',
+      transform: `translate(calc(50% + 1px), calc(((${spacing.xs}px * 2) + 1rem) * -1)) rotate(-90deg)`,
+      border: 'none',
+      cursor: 'pointer',
+      paddingTop: spacing.xs,
+      paddingBottom: spacing.xs,
+      paddingLeft: spacing.sm,
+      paddingRight: spacing.sm,
+      background: colorScheme === 'light' ? white : colors.dark[7],
+      color: 'inherit',
+      outline: 'none',
+      boxShadow: `0 0 7px 0 transparent`,
+      ...transitionBase,
+      '&:hover': {
+        boxShadow: `0 0 7px 3px ${fn.rgba(colorScheme === 'dark' ? colors.gray[1] : colors.dark[7], 0.15)}`,
+      }
+    },
+  }
+});
 
-const IFrameWrapper = ({
-  children,
-  title,
-  src
-}: PropsWithChildren<{ src: string; title: string }>) => {
-  const [open, setOpen] = React.useState(false);
+const IFrameWrapper = (props: PropsWithChildren<{ src: string; title: string; open?: boolean }>) => {
+  const {
+    children,
+    title,
+    src
+  } = props
+  const [open, setOpen] = React.useState(props.open || false);
   const [hover, setHover] = React.useState(false);
   const {classes, cx} = useStyles();
 
@@ -225,6 +222,7 @@ const IFrame = (props: PropTypes) => {
     source,
     mdx,
     tags,
+    open,
   } = props;
   const { classes } = useStyles();
   
@@ -250,7 +248,13 @@ const IFrame = (props: PropTypes) => {
 
   if (!src) return content
 
-  return <IFrameWrapper src={src} title={title}>{content}</IFrameWrapper>
+  return <IFrameWrapper
+    src={src}
+    title={title}
+    open={open}
+  >
+    {content}
+  </IFrameWrapper>
 };
 
 export default IFrame;
