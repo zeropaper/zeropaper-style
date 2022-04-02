ffef8b2a3c37471ff5d78ec685cd798be590b704
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2022-04-02 08:21:17 +0200

    chore(Tina): update and cleanup schema
    - replace excerpt by description
    - remove SEO fields

--- .tina/schema.ts
+++ .tina/schema.ts
@@ -1,23 +1,5 @@
 import { defineSchema, TinaField, TinaTemplate } from '@tinacms/cli'
 
-const ACTIONS: TinaField = {
-  name: 'actions',
-  type: 'object',
-  list: true,
-  fields: [
-    { name: 'label', type: 'string' },
-    { name: 'icon', type: 'string' },
-    { name: 'variant', type: 'string' },
-    { name: 'url', type: 'string' },
-  ],
-}
-
-const defaultFeature = {
-  href: 'https://zeropaper.style',
-  title: "Here's Another Feature",
-  description: "This is where you might talk about the feature, if this wasn't just filler text.",
-}
-
 const feature = {
   label: 'Feature' as const,
   name: 'feature' as const,
@@ -55,83 +37,38 @@ const tagsField: TinaField = {
   }
 }
 
-type MdxTemplate = TinaTemplate & { inline?: boolean }
-
-const mdxDateTime: MdxTemplate = {
-  name: "DateTime",
-  label: "Date & Time",
-  inline: true,
-  fields: [
-    {
-      name: "format",
-      label: "Format",
-      type: "string",
-      options: ["utc", "iso", "local"],
-    },
-  ],
-}
-
-const mdxBlockQuote: MdxTemplate = {
-  name: "BlockQuote",
-  label: "Block Quote",
-  fields: [
-    {
-      name: "children",
-      label: "Quote",
-      type: "rich-text",
-    },
-    {
-      name: "authorName",
-      label: "Author",
-      type: "string",
-    },
-  ],
-}
-
-const mdxNewsletterSignup: MdxTemplate = {
-  name: "NewsletterSignup",
-  label: "Newsletter Sign Up",
-  fields: [
-    {
-      name: "children",
-      label: "CTA",
-      type: "rich-text",
-    },
-    {
-      name: "placeholder",
-      label: "Placeholder",
-      type: "string",
-    },
-    {
-      name: "buttonText",
-      label: "Button Text",
-      type: "string",
-    },
-    {
-      name: "disclaimer",
-      label: "Disclaimer",
-      type: "rich-text",
-    },
-  ],
-  ui: {
-    defaultItem: {
-      placeholder: "Enter your email",
-      buttonText: "Notify Me",
-    },
-  },
-}
-
 const mdxBodyField: TinaField = {
   type: "rich-text",
   label: "Body",
   name: "body",
-  templates: [
-    // mdxDatTime,
-    // mdxBlockQuote,
-    // mdxNewsletterSignup,
-  ],
   isBody: true,
 }
+
+const titleField: TinaField = {
+  type: 'string',
+  name: 'title',
+  label: 'Title',
+}
+const descriptionField: TinaField = {
+  type: 'string',
+  name: 'description',
+  label: 'Description',
+}
+
+const seoFields: TinaField = {
+  type: 'object',
+  name: 'seo',
+  label: 'SEO',
+  fields: [
+    titleField,
+    descriptionField,
+  ],
+};
+
+const pageFields: TinaField[] = [
+  titleField,
+  descriptionField,
+]
 
 export default defineSchema({
   collections: [
@@ -142,16 +79,7 @@ export default defineSchema({
       format: 'json',
       match: '*.json',
       fields: [
-        { type: 'string', name: 'title' },
-        {
-          type: 'object',
-          name: 'seo',
-          label: 'SEO',
-          fields: [
-            { type: 'string', name: 'title' },
-            { type: 'string', name: 'description' },
-          ],
-        },
+        ...pageFields,
         {
           label: 'Blocks',
           name: 'blocks',
@@ -168,38 +96,6 @@ export default defineSchema({
               ],
             },
             feature,
-            // {
-            //   label: 'Features',
-            //   name: 'features',
-            //   fields: [
-            //     { name: 'headline', label: 'Headline', type: 'string' },
-            //     { name: 'subline', label: 'Subline', type: 'string' },
-            //     { name: 'items', label: 'Items', type: 'object', list: true, templates: [feature] },
-            //   ],
-            // },
-            // {
-            //   name: 'flying',
-            //   label: 'Flying',
-            //   fields: [
-            //     { name: 'headline', type: 'string' },
-            //     { name: 'subline', type: 'string' },
-            //     ACTIONS,
-            //     {
-            //       name: 'items',
-            //       type: 'object',
-            //       list: true,
-            //       fields: [
-            //         { name: 'headline', type: 'string' },
-            //         { name: 'subline', type: 'string' },
-            //         {
-            //           name: 'cli',
-            //           type: 'boolean',
-            //           ui: { defaultValue: false },
-            //         },
-            //       ],
-            //     },
-            //   ],
-            // },
           ],
         },
       ],
@@ -211,26 +107,7 @@ export default defineSchema({
       match: '*.mdx',
       format: 'mdx',
       fields: [
-        { type: 'string', name: 'title' },
-        {
-          type: 'object',
-          name: 'seo',
-          label: 'SEO',
-          fields: [
-            // { type: 'string', name: 'title' },
-            { type: 'string', name: 'description' },
-          ],
-        },
-        {
-          type: 'string',
-          label: 'Excerpt',
-          name: 'excerpt',
-        },
-        // {
-        //   type: 'image',
-        //   label: 'Image',
-        //   name: 'ogImage',
-        // },
+        ...pageFields,
         mdxBodyField,
       ],
     },
@@ -253,23 +130,14 @@ export default defineSchema({
       match: '**/*.mdx',
       format: 'mdx',
       fields: [
-        { type: 'string', name: 'title' },
-        { type: 'string', name: 'description' },
+        ...pageFields,
         {
           type: 'string',
           label: 'Slug',
           name: 'slug',
         },
         dateField,
-        {
-          type: 'object',
-          name: 'seo',
-          label: 'SEO',
-          fields: [
-            { type: 'string', name: 'title' },
-            { type: 'string', name: 'description' },
-          ],
-        },
+        // seoFields,
         tagsField,
         {
           type: 'string',
@@ -290,59 +158,14 @@ export default defineSchema({
       path: 'content/blog',
       format: 'mdx',
       fields: [
-        {
-          type: 'string',
-          label: 'Title',
-          name: 'title',
-        },
+        ...pageFields,
         {
           type: 'string',
           label: 'Slug',
           name: 'slug',
         },
-        {
-          type: 'string',
-          label: 'Excerpt',
-          name: 'excerpt',
-        },
         tagsField,
-        // {
-        //   // type: 'string',
-        //   type: 'image',
-        //   label: 'Cover Image',
-        //   name: 'coverImage',
-        // },
         dateField,
-        // {
-        //   type: 'object',
-        //   label: 'Author',
-        //   name: 'author',
-        //   fields: [
-        //     {
-        //       type: 'string',
-        //       label: 'Name',
-        //       name: 'name',
-        //     },
-        //     {
-        //       type: 'string',
-        //       label: 'Picture',
-        //       name: 'picture',
-        //     },
-        //   ],
-        // },
-        // {
-        //   type: 'object',
-        //   label: 'OG Image',
-        //   name: 'ogImage',
-        //   fields: [
-        //     {
-        //       // type: 'string',
-        //       type: 'image',
-        //       label: 'Url',
-        //       name: 'url',
-        //     },
-        //   ],
-        // },
         mdxBodyField,
       ],
     },
