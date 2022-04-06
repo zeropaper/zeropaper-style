ba699cf563afa6fcdef73487dbc41549e55703dc
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2022-04-06 09:00:13 +0200

    style: remove unused import

--- pages/blog/[...slug].tsx
+++ pages/blog/[...slug].tsx
@@ -1,7 +1,6 @@
 import ErrorPage from 'next/error';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { useTina } from 'tinacms/dist/edit-state';
 import { MDXRenderer } from '../../components/MDXRenderer/MDXRenderer';
 import { ExperimentalGetTinaClient } from '../../.tina/__generated__/types';
 
