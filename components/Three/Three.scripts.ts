a0fcef33d3213b6108682b78c1d840c0aaf606a5
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2022-04-06 14:13:24 +0200

    feat(Three): add multiple unfolding cubes

--- components/Three/Three.scripts.ts
+++ components/Three/Three.scripts.ts
@@ -62,13 +62,29 @@ function ensure(name: string, creator: (_THREE: typeof THREE) => THREE.Object3D,
 
 const degToRad = (deg: number) => deg * Math.PI / 180
 
-let fraction = 1;
-let unfold = (f = fraction) => { }
-const ensureOtherCube = (scene: SceneCtx['scene'], recreate = false) => {
-  return ensure('other-cube', (_THREE) => {
-    const group = new THREE.Group();
+class UnfoldingCube extends THREE.Group {
+  constructor() {
+    super()
+    this.group = new THREE.Group()
+    this.add(this.group)
+    this.faces = this.init()
+    this.unfold(0)
+  }
 
-    const distance = degToRad(180)
+  private group: THREE.Group
+
+  private faces: {
+    left: THREE.Mesh,
+    right: THREE.Mesh,
+    top: THREE.Mesh,
+    bottom: THREE.Mesh,
+    front: THREE.Mesh,
+    backGroup: THREE.Group,
+    back: THREE.Mesh,
+  }
+
+  private init() {
+    const { group } = this;
 
     const geometry = new THREE.BoxGeometry(1, 0.01, 1);
     const material = new THREE.MeshStandardMaterial({
@@ -79,29 +95,24 @@ const ensureOtherCube = (scene: SceneCtx['scene'], recreate = false) => {
 
     const bottom = new THREE.Mesh(geometry, material);
 
-    const leftStart = degToRad(-90)
     const left = new THREE.Mesh(geometry.clone(), material);
     left.geometry.translate(0, 0, -0.5);
     left.position.z = -0.5;
 
-    const rightStart = degToRad(-90)
     const right = new THREE.Mesh(geometry.clone(), material);
     right.geometry.translate(0, 0, -0.5);
     right.position.z = 0.5;
 
-    const frontStart = degToRad(-180)
     const front = new THREE.Mesh(geometry.clone(), material);
     front.geometry.translate(0, 0, -0.5);
     front.position.x = -0.5;
 
-    const backGroupStart = degToRad(-90)
     const backGroup = new THREE.Group();
     backGroup.position.x = 0.5;
 
     const back = new THREE.Mesh(geometry.clone(), material);
     back.position.x = 0.5;
 
-    const topStart = degToRad(-90)
     const top = new THREE.Mesh(geometry.clone(), material);
     top.position.x = 1;
     top.geometry.translate(0.5, 0, 0);
@@ -130,18 +141,52 @@ const ensureOtherCube = (scene: SceneCtx['scene'], recreate = false) => {
 
     group.translateY(1 - fraction);
 
-    unfold = (f = fraction) => {
-      left.rotation.set(leftStart + (distance * f), 0, 0)
-      right.rotation.set(rightStart - (distance * f), 0, 0)
-      front.rotation.set(degToRad(90), frontStart - (distance * f), degToRad(90))
-      backGroup.rotation.set(0, 0, backGroupStart + (distance * f))
-      top.rotation.set(0, 0, topStart + (distance * f))
-
-      group.position.set(0, 0, 0)
-      group.translateY((1 - f) - 0.5);
+    return {
+      front,
+      backGroup,
+      back,
+      left,
+      right,
+      top,
+      bottom,
     }
-    unfold(0)
-    return group;
+  }
+
+  unfold = (f = 1) => {
+    const { group } = this;
+    const distance = degToRad(180)
+    const leftStart = degToRad(-90)
+    const rightStart = degToRad(-90)
+    const frontStart = degToRad(-180)
+    const backGroupStart = degToRad(-90)
+    const topStart = degToRad(-90)
+
+    const {
+      left,
+      right,
+      top,
+      // bottom,
+      front,
+      // back,
+      backGroup,
+    } = this.faces;
+
+    left.rotation.set(leftStart + (distance * f), 0, 0)
+    right.rotation.set(rightStart - (distance * f), 0, 0)
+    front.rotation.set(degToRad(90), frontStart - (distance * f), degToRad(90))
+    backGroup.rotation.set(0, 0, backGroupStart + (distance * f))
+    top.rotation.set(0, 0, topStart + (distance * f))
+
+    group.position.set(0, 0, 0)
+    group.translateY((1 - f) - 0.5);
+  }
+}
+
+let fraction = 1;
+// let unfold = (f = fraction) => { }
+const ensureOtherCube = (scene: SceneCtx['scene'], recreate = false, name = 'other-cube') => {
+  return ensure(name, (_THREE) => {
+    return new UnfoldingCube()
   }, scene, recreate);
 }
 
@@ -150,7 +195,7 @@ const ensureOtherCube = (scene: SceneCtx['scene'], recreate = false) => {
 const ensureSceneHelper = (scene: SceneCtx['scene'], recreate = false) => {
   return ensure('scene-helper', (_THREE) => {
     const group = new THREE.Group()
-    const grid = new THREE.GridHelper(5, 10);
+    const grid = new THREE.GridHelper(24, 12);
     group.add(grid)
     const gridX = grid.clone()
     gridX.rotateX(Math.PI / 2)
@@ -173,11 +218,11 @@ const ensureSpotLight = (scene: SceneCtx['scene'], recreate = false) => {
     // scene.add(helper)
     // helper.update()
     instance.castShadow = true;
-    instance.shadow.mapSize.width = 1024;
-    instance.shadow.mapSize.height = 1024;
+    instance.shadow.mapSize.width = 2048 * 2;
+    instance.shadow.mapSize.height = 2048 * 2;
     instance.shadow.camera.near = 1;
-    instance.shadow.camera.far = 50;
-    instance.shadow.focus = 0.1;
+    instance.shadow.camera.far = 500;
+    instance.shadow.focus = 0.5;
     instance.lookAt(scene.position);
     return instance
   }, scene, recreate);
@@ -191,7 +236,7 @@ const ensureShadowCaster = (scene: SceneCtx['scene'], recreate = false) => {
 
     const instance = new THREE.Mesh(geometry, material)
     instance.receiveShadow = true;
-    instance.position.y = -2
+    instance.position.y = -12
     instance.rotation.x = degToRad(-90)
     return instance
   }, scene, recreate);
@@ -214,6 +259,9 @@ const ensureAmbientLight = (scene: SceneCtx['scene'], recreate = false) => {
 //   updateCameraTop(scene);
 // }
 
+const randBool = () => Math.random() > 0.5
+
+let cubes: UnfoldingCube[][][]
 export const onMount = ({ scene, clock }: SceneCtx) => {
   // @ts-ignore
   window.__scene = scene
@@ -222,12 +270,26 @@ export const onMount = ({ scene, clock }: SceneCtx) => {
 
   updateCamera(scene);
 
-  // ensureSceneHelper(scene, true);
+  ensureSceneHelper(scene, true);
   ensureShadowCaster(scene, true);
   ensureAmbientLight(scene, true);
   ensureSpotLight(scene, true);
 
-  ensureOtherCube(scene, true);
+  const dist = 4
+  const w = 3;
+  const h = 3;
+  const d = 3;
+  // const cube = ensureOtherCube(scene, true);
+  cubes = new Array(w).fill(1).map((_, a) => new Array(h).fill(1).map((__, b) => new Array(d).fill(1).map((___, c) => {
+    const cube = ensureOtherCube(scene, true, `clone-${a}-${b}-${c}`) as UnfoldingCube
+    cube.position.x = (w * -2) + (a * dist) + (dist * 0.5)
+    cube.position.y = (h * -2) + (b * dist) + (dist * 0.5)
+    cube.position.z = (d * -2) + (c * dist) + (dist * 0.5)
+    if (randBool()) cube.rotateX(degToRad(randBool() ? -90 : 90))
+    if (randBool()) cube.rotateY(degToRad(randBool() ? -90 : 90))
+    if (randBool()) cube.rotateZ(degToRad(randBool() ? -90 : 90))
+    return cube
+  })))
 };
 
 // let frameCounter = 0;
@@ -249,14 +311,22 @@ export const onRender = (ctx: SceneCtx) => {
   const deltaTime = now - prevNow
 
   const spotLight = ensureSpotLight(scene) as THREE.SpotLight;
-  spotLight.position.y = 20
+  spotLight.position.y = 120
   spotLight.position.x = (Math.cos(now * 0.1) * 5) + 5;
   spotLight.position.z = (Math.sin(now * 0.1) * 5) + 2.5;
   spotLight.lookAt(scene.position);
 
   (scene.getObjectByName('spot-light-helper') as THREE.SpotLightHelper | undefined)?.update();
 
-  unfold(1 - ((now * 0.1) % 1))
+  const fraction = 1 - ((now * 0.1) % 1)
+  cubes.forEach((layer, a) => {
+    layer.forEach((row, b) => {
+      row.forEach((cube, c) => {
+        console.info('...', cube.name, a, b, c)
+        cube.unfold(fraction)
+      })
+    })
+  })
 
   // if (frameCounter >= 60) {
   //   frameCounter = 0;
