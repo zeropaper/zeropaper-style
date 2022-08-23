f4ae0cbbbfcef1b8ecabd8d7dff2b2fdbbdab4b4
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2022-08-23 10:56:04 +0200

    fix(pages): correctly handle the published prop

--- lib/filterUnpublished.ts
+++ lib/filterUnpublished.ts
@@ -1,7 +1,5 @@
-const { NODE_ENV = 'development' } = process.env;
-
 type Publishable = { published?: boolean }
 
 export default function filterUnpublished<T = Publishable>(objs: T[]): T[] {
-  return NODE_ENV !== 'production' ? objs : objs.filter((obj: Publishable) => obj.published);
+  return process.env.NODE_ENV !== 'production' ? objs : objs.filter((obj: Publishable) => obj.published);
 }
