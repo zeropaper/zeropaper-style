4083cdc3c881f6a19f484a3eb27f206c1c5047f3
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2022-08-25 12:19:38 +0200

    feat(LanguagesGraph): add base

--- pages/stuff/index.tsx
+++ pages/stuff/index.tsx
@@ -1,13 +1,15 @@
 import { useState, useEffect, useMemo } from 'react';
 import Head from 'next/head';
 import { ActionIcon, Box, Button, createStyles, Grid, Group, Paper, Select, Text, TextInput, Title } from '@mantine/core';
-import getRepos, { NodeLanguage, RepoInfo } from '../../lib/getRepos';
+import { formatDistanceToNow } from 'date-fns';
+import { IconBrandGithub, IconSortAscending2, IconSortDescending2 } from '@tabler/icons';
+
+import getRepos, { RepoInfo } from '../../lib/getRepos';
 import { LayoutContentWrapper } from '../../components/Layout/Layout';
 import { DraftLink as Link } from '../../components/Link/Link';
 import filterUnpublished from '../../lib/filterUnpublished';
 import getStuffContext from '../../lib/getStuffContext';
 import { AsyncReturnType } from '../../typings';
-import { IconBrandGit, IconBrandGithub, IconGitFork, IconSortAscending2, IconSortDescending2 } from '@tabler/icons';
 
 const useRepoStyles = createStyles(({ spacing }) => ({
   paper: {
@@ -33,17 +35,26 @@ const useRepoStyles = createStyles(({ spacing }) => ({
 function Repo({
   name,
   description,
-  url
+  url,
+  languages,
+  createdAt,
+  updatedAt,
 }: RepoInfo) {
   const { classes } = useRepoStyles();
   return (
-    <Grid.Col span={4}>
+    <Grid.Col sm={6}>
       <Paper className={classes.paper} withBorder p="sm">
         <Title className={classes.title} order={3}>
           {name}
         </Title>
 
-        <Text>{description}</Text>
+        <Group>
+          <Text>C: {formatDistanceToNow(new Date(createdAt), { addSuffix: true })}</Text>
+          <Text>U: {formatDistanceToNow(new Date(updatedAt), { addSuffix: true })}</Text>
+        </Group>
+
+        <Text sx={{ flexGrow: 1 }}>{description}</Text>
+        <Text>{languages.nodes.map(({ name }) => name).join(', ')}</Text>
 
         <Link className={classes.link} unpublished={false} href={url}>
           <IconBrandGithub />
@@ -72,6 +83,54 @@ function languagesFilter(names: string[]) {
     : true;
 }
 
+const useLanguagesGraphStyles = createStyles(({ spacing }) => ({
+  root: {
+    listStyle: 'none',
+    padding: 0,
+    margin: `${spacing.sm}px 0`,
+  },
+  item: {
+    display: 'block',
+    height: spacing.xs,
+    width: '100%',
+  }
+}));
+function LanguagesGraph({
+  languages,
+  repos,
+}: {
+  languages: { name: string; color: string }[];
+  repos: RepoInfo[];
+}) {
+  const { classes } = useLanguagesGraphStyles();
+  return (
+    <Box component='ul' className={classes.root}>
+      {languages.map(({ name, color }) => (
+        <Box
+          className={classes.item}
+          component='li'
+          title={name}
+          key={name}
+          sx={{
+            backgroundColor: color,
+          }}
+        />
+      ))}
+    </Box>
+  )
+}
+
+function findTimeEdges(repos: RepoInfo[]) {
+  let start = Date.now();
+  let end = 0;
+  repos.forEach((repo) => {
+    const ts = new Date(repo.createdAt).getTime();
+    if (ts < start) start = ts;
+    if (ts > end) end = ts;
+  });
+  return [start, end, end - start];
+}
+
 function Repos({
   repos,
   totalCount
@@ -95,6 +154,8 @@ function Repos({
     });
     return Object.values(languages).sort(({ repositories: a }, { repositories: b }) => b.length - a.length);
   }, [repos]);
+  const [start, setStart] = useState(0);
+  const [limit, setLimit] = useState(10);
   const [searched, setSearched] = useState('');
   const [sortOn, setSortOn] = useState<keyof RepoInfo>('updatedAt');
   const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('desc');
@@ -108,25 +169,22 @@ function Repos({
       .filter(languagesFilter(activeLanguages))
       .sort(sortBy(sortOn, sortDirection));
   }, [repos, searched, sortOn, sortDirection, activeLanguages]);
-  const handleToggleLanguage = (language: string) => setActiveLanguages((lngs) => lngs.includes(language) ? lngs.filter((l) => l !== language) : [...lngs, language]);
+  const allTimesEdges = useMemo(() => {
+    return findTimeEdges(repos);
+  }, [repos]);
+  const filteredEdges = useMemo(() => {
+    return findTimeEdges(filtered);
+  }, [filtered]);
 
+  const handleToggleLanguage = (language: string) => setActiveLanguages((lngs) => lngs.includes(language) ? lngs.filter((l) => l !== language) : [...lngs, language]);
   return (
     <>
-      <Group>
-        {allLanguages.map(({ name, repositories }) => (
-          <Button
-            key={name}
-            type="button"
-            variant={activeLanguages.includes(name) ? 'subtle' : 'default'}
-            onClick={() => handleToggleLanguage(name)}
-          >
-            {`${name} (${repositories.length})`}
-          </Button>
-        ))}
-      </Group>
+      <Title order={2}>Git Repositories</Title>
+      <Text component='p'>This list does not contain forks, private repositories or the ones hosted outside of GitHub.</Text>
 
-      <Group>
+      <Group my="sm">
         <TextInput
+          sx={{ flexGrow: 1 }}
           placeholder="Search"
           onChange={(e) => setSearched(e.target.value)}
         />
@@ -146,8 +204,26 @@ function Repos({
         </ActionIcon>
       </Group>
 
-      <Grid justify="center" grow mt="sm">
-        {filtered.slice(0, 20).map(repo => (
+      <Group mb="sm">
+        {allLanguages.map(({ name, repositories }) => (
+          <Button
+            key={name}
+            type="button"
+            variant={activeLanguages.includes(name) ? 'subtle' : 'default'}
+            onClick={() => handleToggleLanguage(name)}
+          >
+            {`${name} (${repositories.length})`}
+          </Button>
+        ))}
+      </Group>
+
+      <LanguagesGraph languages={allLanguages} repos={filtered} />
+      <Text component='p'>
+        {`Earliest: ${formatDistanceToNow(new Date(filteredEdges[0]), { addSuffix: true })} - Latest: ${formatDistanceToNow(new Date(filteredEdges[1]), { addSuffix: true })}`}
+      </Text>
+
+      <Grid justify="center" grow mb="sm">
+        {filtered.slice(start, start + limit).map(repo => (
           <Repo key={repo.name} {...repo} />
         ))}
       </Grid>
@@ -165,24 +241,25 @@ const Stuff = (props: AsyncReturnType<typeof getStaticProps>['props']) => {
       <Box component='header'>
         <Title>Stuff</Title>
 
-        <Text>
-          On this page you can find some stuff I have done or am currently doing.
-          <br />
-          It is quite common for me to try a new idea or tech by creating a Git repository and fiddle with it. This implies that some aspects may not be consistent across all the repositories.
-          <br />
-          Not all is open-source but most is.
+        <Text component='p'>
+          Code is to me like paint. Give my paint and it is going to be a mess, give me a text editor and I will draw you a sheep in CSS.
+        </Text>
+        <Text component='p'>
+          It is quite common that I try a new idea or tech by creating a Git repository and fiddle with it. This implies that some aspects may not be consistent across all the repositories.
         </Text>
       </Box>
 
-      <ul>
+      <Grid justify="center" grow my="sm">
         {props.data.posts.map((post) => (
-          <li key={post.href}>
-            <Link unpublished={!post?.published} href={post.href}>
-              {post.title}
-            </Link>
-          </li>
+          <Grid.Col sm={6} key={post.href}>
+            <Paper withBorder p="sm">
+              <Link unpublished={!post?.published} href={post.href}>
+                {post.title}
+              </Link>
+            </Paper>
+          </Grid.Col>
         ))}
-      </ul>
+      </Grid>
 
       <Repos
         totalCount={props.data.repos.totalCount}
