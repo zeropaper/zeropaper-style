27b0357efe9d1351caedf3028d39f1a691e80e24
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2022-09-13 19:41:35 +0200

    remove timeline from mdxrenderer

--- components/MDXRenderer/MDXRenderer.tsx
+++ components/MDXRenderer/MDXRenderer.tsx
@@ -4,19 +4,14 @@ import {
   Components,
 } from "tinacms/dist/rich-text";
 import Link from "../Link/Link";
-import Timeline from "../Timeline/Timeline";
 
 const mdComponents: Components<{
-  Timeline: {
-    [any: string]: any;
-  };
 }> = {
   a: ({ href, children, ...props }: any) => (
     <Link {...props} href={href || "#"}>
       {children}
     </Link>
   ),
-  Timeline,
 };
 
 export type MDXRendererProps = {
