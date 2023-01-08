cf7c559e9ee953518b3a162fb5b139fc40d52137
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2023-01-08 11:49:13 +0100

    docs: add some comments

--- misc/ts-cheatsheet.ts
+++ misc/ts-cheatsheet.ts
@@ -1,13 +1,16 @@
+/**
+ * This file contains some examples of TypeScript usage that I found useful.
+ */
 type ArrayFlatten<Type> = Type extends Array<infer Item> ? Item : Type;
 
-type UA = 'a' | 'b';
-type UB = 'b' | 'c';
+type UA = "a" | "b";
+type UB = "b" | "c";
 
 type U = UA | UB;
 
-const item: U = 'a';
+const item: U = "a";
 // @ts-ignore
-const invalidItem: U = 'd';
+const invalidItem: U = "d";
 
 type InputType = string[];
 
@@ -18,14 +21,18 @@ const someArray = [1, 2, null];
 const yep: ArrayFlatten<typeof someArray> = 1000;
 const alsoYep: ArrayFlatten<typeof someArray> = null;
 // @ts-ignore
-const nope: ArrayFlatten<typeof someArray> = '1000';
+const nope: ArrayFlatten<typeof someArray> = "1000";
 
+type Attrs<EType> = EType extends HTMLElement
+  ? {
+      [AName in keyof EType]?: EType[AName];
+    }
+  : never;
 
-type Attrs<EType> = EType extends HTMLElement ? {
-  [AName in keyof EType]?: EType[AName];
-} : never;
-
-function create<TName extends keyof HTMLElementTagNameMap>(tagName: TName, attrs: Attrs<HTMLElementTagNameMap[TName]>): HTMLElementTagNameMap[TName] {
+function create<TName extends keyof HTMLElementTagNameMap>(
+  tagName: TName,
+  attrs: Attrs<HTMLElementTagNameMap[TName]>
+): HTMLElementTagNameMap[TName] {
   const el = document.createElement(tagName);
   if (!attrs) return el;
 
@@ -36,16 +43,18 @@ function create<TName extends keyof HTMLElementTagNameMap>(tagName: TName, attrs
   return el;
 }
 
-type AsyncReturnType<T extends (...args: any) => Promise<any>> =
-  T extends (...args: any) => Promise<infer R> ? R : any
+type AsyncReturnType<T extends (...args: any) => Promise<any>> = T extends (
+  ...args: any
+) => Promise<infer R>
+  ? R
+  : any;
 
 async function asyncFn(): Promise<string> {
-  return 'a';
+  return "a";
 }
 
-const returned: AsyncReturnType<typeof asyncFn> = '1';
+const returned: AsyncReturnType<typeof asyncFn> = "1";
 // @ts-ignore
 const notANumber: AsyncReturnType<typeof asyncFn> = 2;
 
-
-export { };
+export {};
