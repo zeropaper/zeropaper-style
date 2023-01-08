927e3b12911f52c1fec932e6ce58793e9fb70121
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2023-01-08 12:19:30 +0100

    fix: correct robots.txt generation

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://zeropaper.style',
-  generateRobotsTxt: true, // (optional)
-  // ...other options
+  siteUrl: process.env.SITE_URL || "https://zeropaper.style",
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        disallow: "/admin/",
+      },
+    ],
+  },
 };
